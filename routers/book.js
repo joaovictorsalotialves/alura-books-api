ba18ff93d9f63ff3d00f0cbc81968876da8c61e9
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-10 13:50:37 -0300

    Rotas de post, patch e delete

--- routers/book.js
+++ routers/book.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.send('Olá Mundo!')
 })
 
+router.post('/', (req, res) => {
+  res.send('Você fez uma requisição POST')
+})
+
+router.patch('/', (req, res) => {
+  res.send('Você fez uma requisição PATCH')
+})
+
+router.delete('/', (req, res) => {
+  res.send('Você fez uma requisição DELETE')
+})
+
 module.exports = router
