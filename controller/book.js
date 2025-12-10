bb17ea6bf2f78ed5eb330d9bd3797fbd9f4e05ec
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-10 14:06:33 -0300

    Get de dados em arquivos

--- controller/book.js
+++ controller/book.js
@@ -1,11 +1,14 @@
+const fs = require('fs')
+
 function getBooks(req, res) {
-    try {
-        res.send('Olá mundo!')
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+  try {
+    const books = JSON.parse(fs.readFileSync('books.json'))
+    res.send(books)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 }
 
 module.exports = {
-    getBooks
+  getBooks
 }
