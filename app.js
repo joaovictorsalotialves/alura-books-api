2ca095340936ba89f876a05c0969a67cd79c9555
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-13 17:31:55 -0300

    Configurando o cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const routerBook = require('./routers/book')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: '*',
+}))
 
 app.use('/book', routerBook)
 
